203ebfd23624cfa2165692b2b9a656024277a82f
SacramentoBaka <sacramentomakhubele@gmail.com>
2024-03-05 21:07:40 +0200

    all variable declared in the js file

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const registerLink = document.querySelector('.btn');
 registerLink.addEventListener('click', () =>{
     logregBox.classList.add('active');
 });
-loginLink.addEventListener('click', () =>{
-    logregBox.classList.remove('active');
-});
+
+
+function displayMonthlyRate() {
+
+    const inputBoxAmount = document.getElementById("inputAmount");
+    const amount = inputBoxAmount.value;
+    const inputBoxRate = document.getElementById("inputInterest");
+    const interestRate = inputBoxRate.value;
+    const inputBoxTerm = document.getElementById("inputYears");
+    const termYears = inputBoxTerm.value;
+
+    loginLink.addEventListener('click', () =>{
+        logregBox.classList.remove('active');
+    });
+  }
